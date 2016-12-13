a0d2a6336c4ef91e45554deb8da8a399b773c74b
wilburrr90 <wilburrr90@gmail.com>
2016-12-12 19:49:38 -0500

    Modify scripts to test various sizes of training sets

--- MATLAB/buildSegmentedClassifiers.m
+++ MATLAB/buildSegmentedClassifiers.m
@@ -7,29 +7,14 @@ selfCategories = {...
     'NE_SELF_S', 'NE_SELF_W', 'NW_SELF_E', 'NW_SELF_S',...
     'SE_SELF_N', 'SE_SELF_W', 'SW_SELF_E', 'SW_SELF_N'...
 };
-mainTrainingSet12 = imageDatastore(fullfile('trainingImages', mainCategories),...
-    'LabelSource', 'foldernames', 'ReadFcn', @firstHalf);
-mainTestSet12 = imageDatastore(fullfile('questionImages', mainCategories),...
-    'LabelSource', 'foldernames', 'ReadFcn', @firstHalf);
-mainNightTestSet12 = imageDatastore(fullfile('nightImages', mainCategories),...
-    'LabelSource', 'foldernames', 'ReadFcn', @firstHalf);
-selfTrainingSet12 = imageDatastore(fullfile('trainingImages', selfCategories),...
-    'LabelSource', 'foldernames', 'ReadFcn', @firstHalf);
-selfTestSet12 = imageDatastore(fullfile('questionImages', selfCategories),...
-    'LabelSource', 'foldernames', 'ReadFcn', @firstHalf);
-selfNightTestSet12 = imageDatastore(fullfile('nightImages', selfCategories),...
-    'LabelSource', 'foldernames', 'ReadFcn', @firstHalf);
 
-% Show two sample images
-subplot(1,2,1);
-imshow(readimage(mainTrainingSet12, 1))
-subplot(1,2,2);
-imshow(readimage(selfTrainingSet12, 9))
+mainTrainingSet = imageDatastore(fullfile('trainingImages', '2', mainCategories),...
+    'LabelSource', 'foldernames', 'ReadFcn', @firstHalf);
+selfTrainingSet = imageDatastore(fullfile('trainingImages', '2', selfCategories),...
+    'LabelSource', 'foldernames', 'ReadFcn', @firstHalf);
 
 % Create a bag of features and a classifier
-mainBag12 = bagOfFeatures(mainTrainingSet12);
-selfBag12 = bagOfFeatures(selfTrainingSet12);
-mainClassifier12 = trainImageCategoryClassifier(mainTrainingSet12, mainBag12);
-selfClassifier12 = trainImageCategoryClassifier(selfTrainingSet12, selfBag12);
-
-
+mainBag = bagOfFeatures(mainTrainingSet);
+selfBag = bagOfFeatures(selfTrainingSet);
+mainClassifier = trainImageCategoryClassifier(mainTrainingSet, mainBag);
+selfClassifier = trainImageCategoryClassifier(selfTrainingSet, selfBag);
--- MATLAB/manualConfusionMatrices.m
+++ MATLAB/manualConfusionMatrices.m
@@ -1,20 +1,28 @@
-% myMainTestSet = mainTestSet;
-% mySelfTestSet = selfTestSet;
-myMainTestSet = mainNightTestSet;
-mySelfTestSet = selfNightTestSet;
+% myMainTestSet = mainAllTestSet12;
+% mySelfTestSet = selfAllTestSet12;
+% myMainTestSet = mainDayTestSet12;
+% mySelfTestSet = selfDayTestSet12;
+myMainTestSet = mainNightTestSet12;
+mySelfTestSet = selfNightTestSet12;
 myLabels = string(mainClassifier.Labels);
 
 l = length(myMainTestSet.Files);
 c = length(mainClassifier.Labels);
+mainCorrect = zeros(l, 1);
+selfCorrect = zeros(l, 1);
 sumCorrect = zeros(l, 1);
 maxCorrect = zeros(l, 1);
-sumGuess = zeros(l, 1);
-maxGuess = zeros(l, 1);
 
 mainGuess = zeros(l, 1);
 selfGuess = zeros(l, 1);
+sumGuess = zeros(l, 1);
+maxGuess = zeros(l, 1);
+
 mainScores = zeros(l, c);
 selfScores = zeros(l, c);
+sumScores = zeros(l, c);
+maxScores = zeros(l, c);
+
 for i=1:l
     [mainG, mainS] = mainClassifier.predict(readimage(myMainTestSet,i));
     [selfG, selfS] = selfClassifier.predict(readimage(mySelfTestSet,i));
@@ -22,6 +30,24 @@ for i=1:l
     mainScores(i,:) = mainS;
     selfGuess(i) = selfG;
     selfScores(i,:) = selfS;
+end
+
+mainConfusionMatrix = zeros(c,c);
+for k=1:length(mainScores)
+    if strcmp(string(mainClassifier.Labels(mainGuess(k))), string(myMainTestSet.Labels(k)))
+        mainCorrect(k) = 1;
+    end
+    q = find(myLabels == string(myMainTestSet.Labels(k)));
+    mainConfusionMatrix(q, mainGuess(k)) = mainConfusionMatrix(q, mainGuess(k)) + 1;
+end
+
+selfConfusionMatrix = zeros(c,c);
+for k=1:length(selfScores)
+    if strcmp(string(mainClassifier.Labels(selfGuess(k))), string(myMainTestSet.Labels(k)))
+        selfCorrect(k) = 1;
+    end
+    q = find(myLabels == string(myMainTestSet.Labels(k)));
+    selfConfusionMatrix(q, selfGuess(k)) = selfConfusionMatrix(q, selfGuess(k)) + 1;
 end
 
 sumScores = mainScores + selfScores;
@@ -58,9 +84,16 @@ end
 
 labelCount = countEachLabel(myMainTestSet);
 for k=1:length(sumConfusionMatrix)
+    mainConfusionMatrix(k,:) = mainConfusionMatrix(k,:) ./ labelCount.Count(k);
+    selfConfusionMatrix(k,:) = selfConfusionMatrix(k,:) ./ labelCount.Count(k);
     sumConfusionMatrix(k,:) = sumConfusionMatrix(k,:) ./ labelCount.Count(k);
     maxConfusionMatrix(k,:) = maxConfusionMatrix(k,:) ./ labelCount.Count(k);
 end
+
+mainConfusionMatrix
+mainPercent = sum(mainCorrect)/length(mainCorrect)
+selfConfusionMatrix
+selfPercent = sum(selfCorrect)/length(selfCorrect)
 sumConfusionMatrix
 sumPercent = sum(sumCorrect)/length(sumCorrect)
 maxConfusionMatrix
